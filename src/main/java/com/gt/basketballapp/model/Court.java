f580cfe302b1c874e040ebb09fe982b1fb815021
luci <purecelucian@gmail.com>
2023-09-20 22:23:00 +0300

    Court class

---
+++ src/main/java/com/gt/basketballapp/model/Court.java
@@ -0,0 +1,41 @@
+package com.gt.basketballapp.model;
+
+import java.awt.geom.Point2D;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Court {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+
+    protected String name;
+    protected Point2D.Double coordinates;
+
+    @Enumerated(EnumType.STRING)
+    protected RenovationStatus renovation_status;
+
+    @Enumerated(EnumType.STRING)
+    protected CourtType court_type;
+
+    protected enum RenovationStatus{
+        RENOVATED,
+        UNDER_RENOVATION,
+        NOT_REVOVATED;
+    }
+    protected enum CourtType{
+        INDOOR,
+        OUTDOOR;
+    }
+}
+
