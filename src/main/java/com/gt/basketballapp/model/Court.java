96a389847fbe97c89b032c17dbc58e600075930d
Ferariu-Adina <ferariuadinac@gmail.com>
2023-09-26 15:41:29 +0300

    Added the changes

--- src/main/java/com/gt/basketballapp/model/Court.java
+++ src/main/java/com/gt/basketballapp/model/Court.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 public class Court {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
