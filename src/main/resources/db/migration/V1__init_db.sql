d2d95850c587df41573fb4d2bf857c73b8977892
Ferariu-Adina <ferariuadinac@gmail.com>
2023-09-28 21:11:22 +0300

    Added the coordinates class

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -2,17 +2,18 @@ CREATE TABLE IF NOT EXISTS court
 ( 
 	id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(255),
-	coordinates POINT,
+	latitude DOUBLE(7,5),
+	longitude DOUBLE(7,5),
 	renovation_status ENUM('RENOVATED','UNDER_RENOVATION', 'NOT_RENOVATED'),
 	court_type ENUM('INDOOR', 'OUTDOOR')
 );
 
-INSERT INTO court(id, name, coordinates, renovation_status, court_type) VALUES
-	(1, 'Sub Tampa/Vointa', POINT(45.64316, 25.59418), 'RENOVATED', 'OUTDOOR'),
-	(2, 'Fartec', POINT(45.66966, 25.59241),'RENOVATED','OUTDOOR'),
-	(3, 'Livada/Schaeffler', POINT(45.64739, 25.58583),'NOT_RENOVATED','OUTDOOR'),
-	(4, 'Triaj', POINT(45.67742, 25.64408),'RENOVATED','OUTDOOR'),
-	(5, 'Agricultorilor', POINT(45.66968, 25.57605),'RENOVATED','OUTDOOR'),
-	(6, 'Stefan cel Mare', POINT(45.64369, 25.63480),'RENOVATED','OUTDOOR'),
-	(7, 'Aluminiului', POINT(45.67135, 25.59787),'RENOVATED','OUTDOOR'),
-	(8, 'Popa Sapca', POINT(45.65250, 25.61752),'RENOVATED','OUTDOOR');
+INSERT INTO court(id, name, latitude, longitude, renovation_status, court_type) VALUES
+	(1, 'Sub Tampa/Vointa', 45.64316, 25.59418, 'RENOVATED', 'OUTDOOR'),
+	(2, 'Fartec', 45.66966, 25.59241,'RENOVATED','OUTDOOR'),
+	(3, 'Livada/Schaeffler', 45.64739, 25.58583,'NOT_RENOVATED','OUTDOOR'),
+	(4, 'Triaj', 45.67742, 25.64408,'RENOVATED','OUTDOOR'),
+	(5, 'Agricultorilor', 45.66968, 25.57605,'RENOVATED','OUTDOOR'),
+	(6, 'Stefan cel Mare', 45.64369, 25.63480,'RENOVATED','OUTDOOR'),
+	(7, 'Aluminiului', 45.67135, 25.59787,'RENOVATED','OUTDOOR'),
+	(8, 'Popa Sapca', 45.65250, 25.61752,'RENOVATED','OUTDOOR');
