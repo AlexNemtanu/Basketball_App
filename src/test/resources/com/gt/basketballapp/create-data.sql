f70a38a50c4f3dd5b98dc89d6a9f7ccb6464569b
daniel <danieldraghicilst@gmail.com>
2023-10-07 00:09:23 +0300

    Fixes

--- src/test/resources/com/gt/basketballapp/create-data.sql
+++ src/test/resources/com/gt/basketballapp/create-data.sql
@@ -4,9 +4,16 @@ CREATE TABLE IF NOT EXISTS court
     name VARCHAR(255),
     latitude DOUBLE(7,5),
     longitude DOUBLE(7,5),
-    renovation_status ENUM('RENOVATED','UNDER_RENOVATION', 'NOT_RENOVATED'),
-    court_type ENUM('INDOOR', 'OUTDOOR')
+    renovation_status VARCHAR(255),
+    court_type VARCHAR(255)
 );
-INSERT INTO COURT(id, name, latitude, longitude, renovation_status, court_type) VALUES ('3','testName1',45.64316, 25.59418, 'INDOOR','UNDER_RENOVATION');
-INSERT INTO COURT(id, name, latitude, longitude, renovation_status, court_type) VALUES ('4','testName2',45.64316, 25.59418, 'OUTDOOR','RENOVATED');
-INSERT INTO COURT(id, name, latitude, longitude, renovation_status, court_type) VALUES ('7','testName3',45.64316, 25.59418, 'OUTDOOR','NOT_RENOVATED');
+
+INSERT INTO court(id, name, latitude, longitude, renovation_status, court_type) VALUES
+                                                                                    (1, 'Sub Tampa/Vointa', 45.64316, 25.59418, 'RENOVATED', 'OUTDOOR'),
+                                                                                    (2, 'Fartec', 45.66966, 25.59241,'RENOVATED','OUTDOOR'),
+                                                                                    (3, 'Livada/Schaeffler', 45.64739, 25.58583,'NOT_RENOVATED','OUTDOOR'),
+                                                                                    (4, 'Triaj', 45.67742, 25.64408,'RENOVATED','OUTDOOR'),
+                                                                                    (5, 'Agricultorilor', 45.66968, 25.57605,'RENOVATED','OUTDOOR'),
+                                                                                    (6, 'Stefan cel Mare', 45.64369, 25.63480,'RENOVATED','OUTDOOR'),
+                                                                                    (7, 'Aluminiului', 45.67135, 25.59787,'RENOVATED','OUTDOOR'),
+                                                                                    (8, 'Popa Sapca', 45.65250, 25.61752,'RENOVATED','OUTDOOR');
